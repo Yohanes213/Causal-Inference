1c0dac99d3424b8b84d446775f983f526c4731fa
Yohanes Teshome Kebede <johnteshe13@gmail.com>
2024-06-12 20:22:42 +0300

    Changed the path of the log file

--- scripts/data_cleaner.py
+++ scripts/data_cleaner.py
@@ -8,7 +8,7 @@ import logging
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
-file_handler = logging.FileHandler('logs/log.log')
+file_handler = logging.FileHandler('log.log')
 file_handler.setLevel(logging.DEBUG)
 
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
