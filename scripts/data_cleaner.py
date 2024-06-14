e779fb9fc18a532ff973dea1c257428f726148fa
Yohanes Teshome Kebede <johnteshe13@gmail.com>
2024-06-14 21:09:25 +0300

    Added remove column function and removed some of the function

--- scripts/data_cleaner.py
+++ scripts/data_cleaner.py
@@ -118,34 +118,6 @@ class DataPipeline:
             logger.error(f"Error converting columns to datetime: {e}")
             return df
         
-    def apply_weekend_holiday(self, df, date_columns):
-        """
-        Applies weekend and holiday checks to specified date columns in a DataFrame 
-        and adds new columns indicating the results.
-
-        Params:
-            df (pd.DataFrame): The DataFrame containing the data.
-            date_columns (list): A list of column names containing date values.
-
-        Returns:
-            pd.DataFrame: The original DataFrame with additional columns for weekend and holiday indicators.
-        """
-        try:
-            df = self.change_datetime(df, date_columns)
-            
-            column = date_columns[1]
-            
-            df[f'is_weekend'] = df[column].apply(self.is_weekend)
-            logger.info(f"Applied weekend check to DataFrame on column {column}")
-            
-            df[f'is_holiday'] = df[column].apply(self.is_holiday)
-            logger.info(f"Applied holiday check to DataFrame on column {column}")
-            
-            logger.info(f"Weekend and holiday checks applied to {len(df)} rows.")
-            return df
-        except Exception as e:
-            logger.error(f"Error applying weekend and holiday checks: {e}")
-            return df
         
     def calculate_distance(self, origin_str, destination_str):
         """
@@ -190,38 +162,17 @@ class DataPipeline:
         except Exception as e:
             logger.error(f"Error Calucation the duration for {start_time} {end_time}: {e}")
 
-    def apply_distance_duration(self, df, origin_cols, destination_cols, start_time_col, end_time_col):
-        """
-        Applies distance and duration calculations to the DataFrame and adds new columns for distance, duration, and speed.
-
-        Params:
-            df (pd.DataFrame): The DataFrame containing the data.
-            origin_cols (list): A list of column names containing origin latitudes and longitudes.
-            destination_cols (list): A list of column names containing destination latitudes and longitudes.
-            start_time_col (str): The column name containing the start time.
-            end_time_col (str): The column name containing the end time.
-
-        Returns:
-            pd.DataFrame: The original DataFrame with additional columns for distance, duration, and speed.
-        """
+    
+    def remove_columns(df, columns):
         try:
-            for origin_col, destination_col in zip(origin_cols, destination_cols):
-                df[f'Distance (km)'] = df.apply(
-                    lambda row: self.calculate_distance(row[origin_col], row[destination_col]), axis=1)
-                logger.info(f"Applied distance calculation to DataFrame for {origin_col} and {destination_col}")
+            df.drop(columns=columns, inplace=True)
 
-            df['Duration (hr)'] = self.calculate_duration(df, start_time_col, end_time_col)
-            logger.info(f"Applied duration calculation to DataFrame using {start_time_col} and {end_time_col}")
-
-            df['Speed (km/hr)'] = df['Distance (km)'] / df['Duration (hr)']
-            logger.info("Calculated speed")
-
-            logger.info(f"Distance, duration, and speed calculations applied to {len(df)} rows.")
+            logger.info(f'Successfully drop {columns}')
             return df
         except Exception as e:
-            logger.error(f"Error applying distance, duration, and speed calculations: {e}")
-            return df
-    
+            logger.error(f"Error dropping {columns} columns")
+
+            
     def impute_elements(df, distance, column):
         try:
             for i, row in df.iterrows():
@@ -258,8 +209,8 @@ class DataPipeline:
             for index, row in df_driver.iterows():
                 order_id = row['order_id']
                 if order_id in df_completed['Trip ID'].unique():
-                    origin_lat = df_completed[df['Trip ID'] == order_id]['Origin Lat'].iloc[0]
-                    origin_lon = df_completed[df['Trip ID'] == order_id]['Origin Lon'].iloc[0]
+                    origin_lat = df_completed[df_completed['Trip ID'] == order_id]['Origin Lat'].iloc[0]
+                    origin_lon = df_completed[df_completed['Trip ID'] == order_id]['Origin Lon'].iloc[0]
 
                     origin = (origin_lat, origin_lon)
 
@@ -273,6 +224,8 @@ class DataPipeline:
         except Exception as e:
             logger.error("Error Calculating the distance from the driver location to trip origin")
 
+
+
 # Usage example
 # if __name__ == "__main__":
 #     pipeline = DataPipeline(country='Nigeria')
@@ -285,24 +238,3 @@ class DataPipeline:
 #         print(df['is_holiday'].value_counts()) 
 #         print(df['is_weekend'].value_counts())  
 
-if __name__ == "__main__":
-    pipeline = DataPipeline('Nigeria')
-    df = pipeline.read_data('data/nb.csv')
-
-    if df is not None:
-        # Define the columns for datetime conversion and weekend/holiday checks
-        date_columns = ['Trip Start Time', 'Trip End Time']
-        df = pipeline.apply_weekend_holiday(df, date_columns)
-        
-        # Define columns for distance and duration calculation
-        origin_cols = ['Trip Origin']
-        destination_cols = ['Trip Destination']
-        # start_time_col = 'start_time'
-        # end_time_col = 'end_time'
-
-        df = pipeline.apply_distance_duration(df, origin_cols, destination_cols, date_columns[0], date_columns[1])
-
-        pipeline.save_data(df, 'data/cleaned_nb.csv')
-        #logger.info("Distance, duration, and speed calculations have been applied to the DataFrame")
-        print(df.head())  # Display the first few rows of the DataFrame to verify changes
-        print(df.info())
